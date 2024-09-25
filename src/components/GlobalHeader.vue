c6e84caaa3b4a5c9bfcaee9c9a417c9147fef802
becoze <liyuanliang20@outlook.com>
2024-09-25 17:41:22 +1000

    feat add routes class for better routing use, feat nav bar correct correctly display

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="globalHeader">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
+    <a-menu
+      mode="horizontal"
+      :selected-keys="selectedKeys"
+      @menu-item-click="doMenuClick"
+    >
       <a-menu-item
         key="0"
         :style="{ padding: 0, marginRight: '38px' }"
@@ -11,15 +15,36 @@
           <div class="title">Becoze OJ</div>
         </div>
       </a-menu-item>
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">Solution</a-menu-item>
-      <a-menu-item key="3">Cloud Service</a-menu-item>
-      <a-menu-item key="4">Cooperation</a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path"
+        >{{ item.name }}
+      </a-menu-item>
     </a-menu>
   </div>
 </template>
 
-<script lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+
+/**
+ * Record and change the value of "current page (the selectedKeys)" on each page routing, to make the nav bar display match the page routing.
+ */
+// Set selectedKeys (the nav bar) to "/" the home page by default
+const selectedKeys = ref(["/"]);
+// Update selectedKeys (the nav bar) after each page route (navigating)
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
