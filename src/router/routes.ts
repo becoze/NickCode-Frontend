0db7dd412cb7055ccf004f906915886f8285382f
becoze <liyuanliang20@outlook.com>
2024-10-21 23:08:46 +1100

    add new routes for pages

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,11 +1,21 @@
 import { RouteRecordRaw } from "vue-router";
 import HomeView from "@/views/HomeView.vue";
+import AdminView from "@/views/AdminView.vue";
+import NoAuthView from "@/views/NoAuthView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
   {
     path: "/",
     name: "Problems",
     component: HomeView,
+  },
+  {
+    path: "/admin",
+    name: "Admin Only",
+    component: AdminView,
+    meta: {
+      access: "canAdmin", // canAdmin, (canUser)
+    },
   },
   {
     path: "/about",
@@ -15,5 +25,10 @@ export const routes: Array<RouteRecordRaw> = [
     // which is lazy-loaded when the route is visited.
     component: () =>
       import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+  {
+    path: "/noAuth",
+    name: "noAuth",
+    component: NoAuthView,
   },
 ];
---
+++ src/views/NoAuthView.vue
@@ -0,0 +1,5 @@
+<template>
+  <div class="no-auth">
+    No auth
+  </div>
+</template>
