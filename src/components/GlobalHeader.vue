7356366f49f7ab33d3d7e915f28c690a9f33a1a2
becoze <liyuanliang20@outlook.com>
2024-11-06 23:40:33 +1100

    feat: style basic layout and components

---
+++ src/components/GlobalHeader.vue
@@ -0,0 +1,39 @@
+<template>
+  <div id="globalHeader">
+    <a-menu mode="horizontal" :default-selected-keys="['1']">
+      <a-menu-item
+        key="0"
+        :style="{ padding: 0, marginRight: '38px' }"
+        disabled
+      >
+        <div class="title-bar">
+          <img class="logo" src="../assets/Stardew_Chicken.png" />
+          <div class="title">Becoze OJ</div>
+        </div>
+      </a-menu-item>
+      <a-menu-item key="1">Home</a-menu-item>
+      <a-menu-item key="2">Solution</a-menu-item>
+      <a-menu-item key="3">Cloud Service</a-menu-item>
+      <a-menu-item key="4">Cooperation</a-menu-item>
+    </a-menu>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.title-bar {
+  display: flex;
+  align-items: center;
+}
+
+.logo {
+  height: 48px;
+}
+
+.title {
+  color: #444;
+  margin-left: 16px;
+}
+</style>
---
+++ src/layouts/BasicLayout.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="basiclayout">
+    <a-layout style="min-height: 100vh">
+      <a-layout-header class="header">
+        <GlobalHeader />
+      </a-layout-header>
+
+      <a-layout-content class="content">
+        <router-view />
+      </a-layout-content>
+
+      <a-layout-footer class="footer">
+        <a href="https://becoze.github.io/wiki" target="_blank"> Becoze </a>
+      </a-layout-footer>
+    </a-layout>
+  </div>
+</template>
+
+<style scoped>
+#basiclayout {
+}
+
+#basiclayout .header {
+  background: #42b983;
+  margin-bottom: 16px;
+}
+
+#basiclayout .content {
+  background: linear-gradient(to right, #eee, #fff);
+  margin-bottom: 16px;
+}
+
+#basiclayout .footer {
+  background: #efefef;
+  padding: 16px;
+  position: sticky;
+  left: 0;
+  right: 0;
+  text-align: center;
+}
+</style>
+<script setup lang="ts">
+import GlobalHeader from "@/components/GlobalHeader.vue";
+</script>
