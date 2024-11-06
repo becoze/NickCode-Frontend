a505a7e057c357e182ff66ca008bd51d01229a2f
becoze <liyuanliang20@outlook.com>
2024-11-07 00:28:59 +1100

    ci: upgrate router and implement dynamic nav bar

--- src/components/GlobalHeader.vue
+++ src/components/GlobalHeader.vue
@@ -1,6 +1,10 @@
 <template>
   <div id="globalHeader">
-    <a-menu mode="horizontal" :default-selected-keys="['1']">
+    <a-menu
+      mode="horizontal"
+      :selected-keys="selectedKeys"
+      @menu-item-click="doMenuClick"
+    >
       <a-menu-item
         key="0"
         :style="{ padding: 0, marginRight: '38px' }"
@@ -11,15 +15,31 @@
           <div class="title">Becoze OJ</div>
         </div>
       </a-menu-item>
-      <a-menu-item key="1">Home</a-menu-item>
-      <a-menu-item key="2">Solution</a-menu-item>
-      <a-menu-item key="3">Cloud Service</a-menu-item>
-      <a-menu-item key="4">Cooperation</a-menu-item>
+      <a-menu-item v-for="item in routes" :key="item.path"
+        >{{ item.name }}
+      </a-menu-item>
     </a-menu>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { routes } from "@/router/routes";
+import { useRouter } from "vue-router";
+import { ref } from "vue";
+
+const router = useRouter();
+const doMenuClick = (key: string) => {
+  router.push({
+    path: key,
+  });
+};
+
+// Set default redirect page as homepage, and remember current page after each page route, for use stay current page after refresh
+const selectedKeys = ref(["/"]);
+router.afterEach((to, from, failure) => {
+  selectedKeys.value = [to.path];
+});
+</script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
