26aab8445e5e32aa41f3ab646bda9ae608e3ec10
becoze <liyuanliang20@outlook.com>
2026-02-05 20:28:33 +1100

    feat: implement Makedown Editor (bytemd) and Code Editor (monaco) interface, basic fucntion and simple value transfer

---
+++ src/components/CodeEditor.vue
@@ -0,0 +1,69 @@
+<template>
+  <!-- Main container for the code editor component -->
+  <div id="code-editor" ref="codeEditorRef" style="min-height: 400px" />
+  <!-- Button to trigger the fillValue function -->
+  <!--  <a-button @change="">submit</a-button>-->
+</template>
+
+<script setup lang="ts">
+// Import Monaco Editor and Vue composition API
+import * as monaco from "monaco-editor";
+import { onMounted, ref, toRaw, withDefaults, defineProps } from "vue";
+
+/**
+ * Interface defining the component properties types
+ * @property value - The initial value of the code editor
+ * @property handleChange - Callback function to handle value changes
+ */
+interface Props {
+  value: string;
+  language?: string;
+  handleChange: (v: string) => void;
+}
+
+/**
+ * Default value for this component
+ */
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  language: () => "java",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+
+// Reference to the DOM element where the editor will be mounted
+const codeEditorRef = ref();
+// Reference to the Monaco Editor instance
+const codeEditor = ref();
+
+/**
+ * Lifecycle hook that runs after the component is mounted
+ * Initializes the Monaco Editor with specified configuration
+ */
+onMounted(() => {
+  if (!codeEditorRef.value) {
+    return;
+  }
+  // Create and configure the Monaco Editor instance
+  codeEditor.value = monaco.editor.create(codeEditorRef.value, {
+    value: props.value,
+    language: "java",
+    automaticLayout: true,
+    minimap: {
+      enabled: true,
+      scale: 6,
+    },
+    colorDecorators: true,
+    readOnly: false,
+    theme: "vs-dark",
+  });
+
+  // Event listener for editor content changes
+  codeEditor.value.onDidChangeModelContent(() => {
+    props.handleChange(toRaw(codeEditor.value).getValue());
+  });
+});
+</script>
+
+<style scoped></style>
---
+++ src/components/MdEditor.vue
@@ -0,0 +1,35 @@
+<template>
+  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+</template>
+
+<script setup lang="ts">
+import gfm from "@bytemd/plugin-gfm";
+import highlight from "@bytemd/plugin-highlight";
+import { Editor, Viewer } from "@bytemd/vue-next";
+import { withDefaults, defineProps } from "vue";
+
+/**
+ * Define components value type
+ */
+interface Props {
+  value: string;
+  handleChange: (v: string) => void;
+}
+
+const plugins = [
+  gfm(),
+  highlight(),
+  // Add more plugins here
+];
+/**
+ * Default value for this component
+ */
+const props = withDefaults(defineProps<Props>(), {
+  value: () => "",
+  handleChange: (v: string) => {
+    console.log(v);
+  },
+});
+</script>
+
+<style scoped></style>
