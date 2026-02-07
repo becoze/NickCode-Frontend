a4392e6bf58fcaf9a5a906ffc7103c58865732de
becoze <liyuanliang20@outlook.com>
2026-02-07 21:51:16 +1100

    feat: enable to pass variable to main (this) components; and some style change

--- src/components/MdEditor.vue
+++ src/components/MdEditor.vue
@@ -1,11 +1,16 @@
 <template>
-  <Editor :value="value" :plugins="plugins" @change="handleChange" />
+  <Editor
+    :value="value"
+    :mode="mode"
+    :plugins="plugins"
+    @change="handleChange"
+  />
 </template>
 
 <script setup lang="ts">
 import gfm from "@bytemd/plugin-gfm";
 import highlight from "@bytemd/plugin-highlight";
-import { Editor, Viewer } from "@bytemd/vue-next";
+import { Editor } from "@bytemd/vue-next";
 import { withDefaults, defineProps } from "vue";
 
 /**
@@ -13,6 +18,7 @@ import { withDefaults, defineProps } from "vue";
  */
 interface Props {
   value: string;
+  mode: string;
   handleChange: (v: string) => void;
 }
 
@@ -26,10 +32,15 @@ const plugins = [
  */
 const props = withDefaults(defineProps<Props>(), {
   value: () => "",
+  mode: () => "split",
   handleChange: (v: string) => {
     console.log(v);
   },
 });
 </script>
 
-<style scoped></style>
+<style>
+.bytemd-toolbar-icon.bytemd-tippy.bytemd-tippy-right:last-child {
+  display: none;
+}
+</style>
