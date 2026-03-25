0923700b597ab53b8f9e1cdaf2be09b5e672af11
becoze <liyuanliang20@outlook.com>
2026-03-25 23:26:34 +1100

    feat: new my submission page route, and fix page access control

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -1,14 +1,13 @@
 import { RouteRecordRaw } from "vue-router";
-import HomeView from "@/views/HomeView.vue";
 import NoAuthView from "@/views/NoAuthView.vue";
 import ACCESS_ENUM from "@/access/accessEnum";
-import component from "*.vue";
 import UserLoginView from "@/views/user/UserLoginView.vue";
 import UserRegisterView from "@/views/user/UserRegisterView.vue";
 import UserLayout from "@/layouts/UserLayout.vue";
 import AddProblemView from "@/views/problem/AddProblemView.vue";
 import ManageProblemView from "@/views/problem/ManageProblemView.vue";
 import ProblemsView from "@/views/problem/ProblemsView.vue";
+import ProblemSubmitView from "@/views/problem/ProblemSubmitView.vue";
 import ViewProblemsView from "@/views/problem/ViewProblemsView.vue";
 
 export const routes: Array<RouteRecordRaw> = [
@@ -38,12 +37,20 @@ export const routes: Array<RouteRecordRaw> = [
     component: ProblemsView,
   },
   {
+    path: "/problem_submit",
+    name: "My Submission",
+    component: ProblemSubmitView,
+    meta: {
+      access: [ACCESS_ENUM.ADMIN, ACCESS_ENUM.USER], // admin / user
+    },
+  },
+  {
     path: "/view/problem/:id",
     name: "View Problem",
     component: ViewProblemsView,
     props: true,
     meta: {
-      access: ACCESS_ENUM.ADMIN, // admin / user
+      access: [ACCESS_ENUM.ADMIN, ACCESS_ENUM.USER], // admin / user
       hideInMenu: true,
     },
   },
@@ -52,7 +59,7 @@ export const routes: Array<RouteRecordRaw> = [
     name: "Add Problem",
     component: AddProblemView,
     meta: {
-      access: ACCESS_ENUM.ADMIN, // admin / user
+      access: [ACCESS_ENUM.ADMIN, ACCESS_ENUM.USER], // admin / user
     },
   },
   {
@@ -60,7 +67,7 @@ export const routes: Array<RouteRecordRaw> = [
     name: "Update Problem",
     component: AddProblemView,
     meta: {
-      access: ACCESS_ENUM.ADMIN, // admin / user
+      access: [ACCESS_ENUM.ADMIN, ACCESS_ENUM.USER], // admin / user
       hideInMenu: true,
     },
   },
@@ -69,7 +76,7 @@ export const routes: Array<RouteRecordRaw> = [
     name: "Manage Problem",
     component: ManageProblemView,
     meta: {
-      access: ACCESS_ENUM.ADMIN, // admin / user
+      access: ACCESS_ENUM.ADMIN, // admin only
     },
   },
   {
@@ -82,7 +89,7 @@ export const routes: Array<RouteRecordRaw> = [
   },
   {
     path: "/about",
-    name: "About me",
+    name: "To GitHub",
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
